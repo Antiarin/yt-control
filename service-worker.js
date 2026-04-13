ee415be6cfe22c1e0a2a08fd442deb19b0a71d5f
Antiarin <chandola.aaryann@gmail.com>
2026-04-14 02:27:39 +0530

    feat: service worker sets default toggle values on install

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,27 @@
 // YT Control - Service Worker
 // Initializes default settings on install
+
+const DEFAULTS = {
+  masterEnabled: true,
+  homeFeed: true,
+  shortsShelf: true,
+  trending: true,
+  shortsTab: true,
+  subscriptions: true,
+  notifications: true,
+  relatedVideos: true,
+  comments: true,
+  autoplay: true,
+  endScreen: true,
+  liveChat: true,
+  searchSuggestions: true,
+  thumbnails: true,
+  voiceSearch: true
+};
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.get(null, (existing) => {
+    const merged = { ...DEFAULTS, ...existing };
+    chrome.storage.sync.set(merged);
+  });
+});
